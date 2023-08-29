d8031f0852c1f2efa10da539095df1e672621632
43719725863 <lucas.bighi@apoioprodesp.sp.gov.br>
2023-08-29 11:07:42 -0300

    init movie detail v2

--- TheMovieDB/Modules/MovieDetail/MovieDetailView.swift
+++ TheMovieDB/Modules/MovieDetail/MovieDetailView.swift
@@ -22,76 +22,95 @@ struct MovieDetailView: View {
     
     var body: some View {
         ScrollView {
-            ZStack(alignment: .top) {
+            VStack {
                 ImageAsync(
                     placeholder: Image("backdrop-w300-placeholder"),
                     fetcher: viewModel.fetchBackdropData
                 )
-                .overlay(alignment: .topTrailing, content: {
-                    Button(action: onClose) {
-                        Image(systemName: "xmark.circle.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(.white)
-                            .padding(10)
+                VStack {
+                    HStack {
+                        ImageAsync(
+                            placeholder: Image("poster-placeholder"),
+                            fetcher: { await viewModel.fetchPosterData(size: .w154) }
+                        )
+                        .cornerRadius(10)
+                        .frame(width: 100)
+                        .padding(.top, -50)
+                        
+                        Text("**\(viewModel.movieTitle)** (\(viewModel.movieReleaseYear))")
+                            .font(.system(size: 18))
+                            .padding(20)
                     }
-                })
-                
+                    genres
+                    overview
+                    overview
+                    overview
+                    overview
+                    overview
+                }
+                .frame(maxWidth: .infinity)
+                .background(.green)
+                .cornerRadius(20)
+                .padding(.top, -25)
+            }
+        }
+        .ignoresSafeArea()
+    }
+    
+    var oldBody: some View {
+        
+        ZStack(alignment: .top) {
+            ImageAsync(
+                placeholder: Image("backdrop-w300-placeholder"),
+                fetcher: viewModel.fetchBackdropData
+            )
+            .overlay(alignment: .topTrailing, content: {
+                Button(action: onClose) {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.white)
+                        .padding(10)
+                }
+            })
+            ScrollView {
                 VStack(alignment: .leading) {
                     Spacer(minLength: 150)
                     ZStack {
-                        HStack {
-                            Text("**\(viewModel.movieTitle)** \(viewModel.movieReleaseYear)")
+                        VStack(alignment: .leading) {
+                            Text("**\(viewModel.movieTitle)** (\(viewModel.movieReleaseYear))")
                                 .font(.system(size: 18))
                                 .frame(width: 200)
+                            FiveStarView(rating: Decimal(viewModel.movieRating))
+                                .frame(width: 80, height: 15, alignment: .leading)
                         }
                         .frame(maxWidth: .infinity, alignment: .trailing)
                         .padding(20)
                         .background(Color("moviedetail.background"))
                         .cornerRadius(20)
                         
-                        HStack {
-                            ImageAsync(
-                                placeholder: Image("poster-placeholder"),
-                                fetcher: { await viewModel.fetchPosterData(size: .w154) }
-                            )
-                            .frame(width: 100)
-                            .cornerRadius(10)
-                        }
+                        ImageAsync(
+                            placeholder: Image("poster-placeholder"),
+                            fetcher: { await viewModel.fetchPosterData(size: .w154) }
+                        )
+                        .frame(width: 100)
+                        .cornerRadius(10)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding([.leading, .bottom], 20)
-                        .padding(.top, 10)
                     }
-                        
-                    FiveStarView(rating: Decimal(viewModel.movieRating))
-                        .frame(width: 80, height: 15, alignment: .leading)
-                        .padding(.leading, 20)
                     
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(viewModel.genres) { genre in
-                                label(of: genre)
-                            }
-                        }
-                        .padding(20)
-                    }
-                    Text("Storyline")
-                        .font(.system(size: 18, weight: .heavy))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 20)
-                    
-                    Text(viewModel.movieOverview)
-                        .font(.system(size: 16))
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 5)
+                    genres
+                    overview
+                    overview
+                    overview
                     Spacer()
                 }
             }
-            .task {
-                await viewModel.getMoviesGenres()
-            }
-            .background(Color("moviedetail.background"))
         }
+        .task {
+            await viewModel.getMoviesGenres()
+        }
+        .background(Color("moviedetail.background"))
         .ignoresSafeArea()
     }
     
@@ -103,6 +122,31 @@ struct MovieDetailView: View {
             .background(Color("moviedetail.genre.foreground"))
             .cornerRadius(40)
     }
+    
+    private var genres: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(viewModel.genres) { genre in
+                    label(of: genre)
+                }
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+    
+    private var overview: some View {
+        Group {
+            Text("Storyline")
+                .font(.system(size: 18, weight: .heavy))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+            
+            Text(viewModel.movieOverview)
+                .font(.system(size: 16))
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+        }
+    }
 }
 
 struct MovieDetailView_Previews: PreviewProvider {
@@ -111,6 +155,6 @@ struct MovieDetailView_Previews: PreviewProvider {
         let viewModel = MovieViewModel(movie: movie)
         viewModel.genres = GenreListResponse.fromLocalJSON.genres
         return MovieDetailView(viewModel: viewModel) {}
-//            .preferredColorScheme(.dark)
+        //            .preferredColorScheme(.dark)
     }
 }
